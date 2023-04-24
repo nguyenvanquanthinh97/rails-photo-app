bc05684bb70d11383cc43180b0c7e761bd3c6514
thinhvqnguyen <thinh.nvq.js220597@gmail.com>
2023-04-24 12:58:41 +0700

    add twitter bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,17 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+import Rails from "@rails/ujs";
+import Turbolinks from "turbolinks";
+import * as ActiveStorage from "@rails/activestorage";
+import "channels";
+import "twitter/bootstrap";
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+Rails.start();
+Turbolinks.start();
+ActiveStorage.start();
+
+document.addEventListener("turbolinks:load", () => {
+  $("a[rel~=popover], .has-popover").popover();
+  $("a[rel~=tooltip], .has-tooltip").tooltip();
+});
