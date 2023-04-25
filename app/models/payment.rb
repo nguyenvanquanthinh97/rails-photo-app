152c84bbb6d1dfd6d6738c65ea00f976a76765f9
thinhvqnguyen <thinh.nvq.js220597@gmail.com>
2023-04-25 18:35:32 +0700

    integrate strip account payment to sign_up

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -3,7 +3,7 @@ class Payment < ApplicationRecord
   belongs_to :user
 
   def self.month_options
-    Date::MONTHNAMES.compact.each_with_index {|name, i| ["#{i+1} - #{name}", i+1]}
+    Date::MONTHNAMES.compact.each_with_index.map {|name, i| ["#{i+1} - #{name}", i+1]}
   end
 
   def self.year_options
@@ -19,7 +19,7 @@ class Payment < ApplicationRecord
       customer: customer.id,
       amount: 1000,
       description: 'Premium',
-      currency: 'USD'
+      currency: 'AUD'
     })
   end
 end
